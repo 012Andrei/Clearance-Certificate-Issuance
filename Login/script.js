3a8c9fbd2818959de01de9495dc53f55c9d604e9
012Andrei <abesamisaj8@gmail.com>
2024-09-14 23:18:57 +0800

    folder para sa user

--- Login/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.nav-item.active').classList.remove('active');
-        item.classList.add('active');
-    });
-});
